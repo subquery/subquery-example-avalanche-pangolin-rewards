95d3485fe1a1f618f4f68fb3e197bff5e66a9a6b
James Bayly <bayly.james@gmail.com>
2022-06-26 23:07:14 +0800

    First Commit

---
+++ src/mappings/mappingHandlers.ts
@@ -0,0 +1,19 @@
+import { PangolinApproval } from "../types";
+import { AvalancheLog } from "@subql/types-avalanche";
+
+export async function handleLog(event: AvalancheLog): Promise<void> {
+  const pangolinApprovalRecord = new PangolinApproval(
+    `${event.blockHash}-${event.logIndex}`
+  );
+
+  pangolinApprovalRecord.transactionHash = event.transactionHash;
+  pangolinApprovalRecord.blockHash = event.blockHash;
+  pangolinApprovalRecord.blockNumber = BigInt(event.blockNumber);
+
+  // topics store data as an array
+  pangolinApprovalRecord.addressFrom = event.topics[0];
+  pangolinApprovalRecord.addressTo = event.topics[1];
+  pangolinApprovalRecord.amount = BigInt(event.topics[2]);
+
+  await pangolinApprovalRecord.save();
+}
