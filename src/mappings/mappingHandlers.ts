9e1996520a30bf2f9d417853156592ae0653683b
James Bayly <bayly.james@gmail.com>
2022-07-07 14:46:44 +1200

    Update start block

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -3,10 +3,7 @@ import { AvalancheLog } from "@subql/types-avalanche";
 
 export async function handleLog(event: AvalancheLog): Promise<void> {
   const { args } = event;
-  // logger.info("Event found " + JSON.stringify(event));
   if (args) {
-    // logger.info(JSON.stringify(args));
-
     const pangolinRewardRecord = new PangolinRewards(
       `${event.blockHash}-${event.logIndex}`
     );
