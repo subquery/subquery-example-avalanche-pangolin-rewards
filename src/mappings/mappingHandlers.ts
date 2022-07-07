7e0a7287ad2fd4a7d88c3c558aa59bb04f633889
James Bayly <46693720+jamesbayly@users.noreply.github.com>
2022-07-07 15:00:17 +1200

    Reward event (#3)
    * Index Reward Events
    
    * Update Staking Contract
    
    * Update build command, remove logging
    
    * Update start block
    
    * Update start block
    
    Co-authored-by: Scott Twiname <skott.twiname@gmail.com>

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,21 +1,20 @@
-import { PangolinApproval } from "../types";
+import { PangolinRewards } from "../types";
 import { AvalancheLog } from "@subql/types-avalanche";
 
 export async function handleLog(event: AvalancheLog): Promise<void> {
-  const pangolinApprovalRecord = new PangolinApproval(
-    `${event.blockHash}-${event.logIndex}`
-  );
-
-  pangolinApprovalRecord.transactionHash = event.transactionHash;
-  pangolinApprovalRecord.blockHash = event.blockHash;
-  pangolinApprovalRecord.blockNumber = BigInt(event.blockNumber);
-
   const { args } = event;
   if (args) {
-    pangolinApprovalRecord.owner = args.owner;
-    pangolinApprovalRecord.spender = args.spender;
-    pangolinApprovalRecord.amount = BigInt(args.value.toString());
-  }
+    const pangolinRewardRecord = new PangolinRewards(
+      `${event.blockHash}-${event.logIndex}`
+    );
 
-  await pangolinApprovalRecord.save();
+    pangolinRewardRecord.transactionHash = event.transactionHash;
+    pangolinRewardRecord.blockHash = event.blockHash;
+    pangolinRewardRecord.blockNumber = BigInt(event.blockNumber);
+
+    pangolinRewardRecord.receiver = args.user;
+    pangolinRewardRecord.amount = BigInt(args.reward.toString());
+
+    await pangolinRewardRecord.save();
+  }
 }
